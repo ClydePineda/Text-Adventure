4cd026f0454cb0b7f0be79153d1a6d6941bd4a83
Clyde Pineda <27971415+ClydePineda@users.noreply.github.com>
2019-09-19 14:18:15 -0700

    added more to player

--- game.java
+++ game.java
@@ -1,11 +1,28 @@
 /**
  * game
  */
+import java.util.Scanner;
+
 public class game {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
         player p = new player();
-        System.out.println(p.name);
+        try {
+            System.out.println("Enter Name:");
+            p.name = scan.nextLine();
+            System.out.println("Enter Age:");
+            p.age = scan.nextInt();
+            System.out.println("Enter Weight (lbs):");
+            p.weight = scan.nextDouble();
+            System.out.println("Enter Height (ft):");
+            int feet = scan.nextInt();
+            System.out.println("Enter Height (in)");
+            int inch = scan.nextInt();
+            p.height = feet +"ft " + inch + "in" ;
+            System.out.println("Hello " + p.name +" your age is " + p.age + " weigh  " + p.weight + "lbs"  + " and " + p.height + " tall.");
+        } finally{
+            scan.close();
+        }
     }
 }
--- player.java
+++ player.java
@@ -3,5 +3,10 @@
  */
 public class player {
 
-    String name = "John Doe";
+    String name = "";
+    int age = 0;
+    double weight = 0;
+    String height = "";
+    double money = 0;
+    // TODO inventory
 }
